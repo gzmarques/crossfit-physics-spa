b439af9ce196a43a182f36c150a00db3a7c0f5ac
gzmarques <gzmarques90@gmail.com>
2026-08-30 18:34:47 -0300

    fix: ajusta chrome na maquina virtual do actions

--- scripts/scraper/index.js
+++ scripts/scraper/index.js
@@ -279,8 +279,12 @@ async function colherLinksDeTreinos(siteKey) {
   if (!alvo) return [];
 
   console.log(`🕷️ Iniciando Spider na página índice: ${alvo.urlIndice}`);
+
+  const browser = await puppeteer.launch({
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
+  });
   
-  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   
   try {
